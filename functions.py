b7ddf74565957e4d2c0ca1376816b162f1445270
Ryan Verveda <76929422+rverveda@users.noreply.github.com>
2022-10-20 22:25:16 -0600

    Added PvP
    Can now play against another player or a computer. Changed how the current player is stored meaning less code is repeated for no need

--- functions.py
+++ functions.py
@@ -10,50 +10,75 @@ def gameStart():
     clear()
     # Have user select their pokemon
     print('Welcome Trainer! Select your pokemon:\n')
-    userInput = input('1. Bulbasaur\n2. Charmander\n3. Squirtle\nEnter your chosen pokemon\'s number: ')
-    if userInput == '1':
-        userPokemon = 'Bulbasaur'
-    elif userInput == '2':
-        userPokemon = 'Charmander'
-    elif userInput == '3':
-        userPokemon = 'Squirtle'
+    p1Input = input('1. Bulbasaur\n2. Charmander\n3. Squirtle\nEnter your chosen pokemon\'s number: ')
+    if p1Input == '1':
+        p1Pokemon = 'Bulbasaur'
+    elif p1Input == '2':
+        p1Pokemon = 'Charmander'
+    elif p1Input == '3':
+        p1Pokemon = 'Squirtle'
     else:
         battleStart = False
-        userPokemon = 0
-        compPokemon = 0
+        p1Pokemon = 0
+        p2Pokemon = 0
     
     # Confirm the pokemon selection
-    if userInput == '1' or userInput == '2' or userInput == '3':
+    if p1Pokemon != 0:
         clear()
-        print('Ah yes. You chose ' + userPokemon + '! What a good choice.')
+        print('Ah yes. You chose ' + p1Pokemon + '! What a good choice.')
         userInput = input('\nAre you ready to battle? yes(y) or no(n):')
         if userInput.lower() == 'yes' or userInput.lower() == 'y':
-            userPokemon, compPokemon = pickCompPokemon(userPokemon)
+            p1Pokemon = pokemon(p1Pokemon)
+            p1Pokemon.isComputer = False
+            p1Pokemon.playerName = 'Trainer'
+            validOpponent = False
+            while validOpponent == False:
+                clear()
+                print('Who are you battling?\n')
+                opponent = int(input('1. Your rival\n2. Computer\nEnter your choice\'s number: '))
+                if opponent == 1 or opponent == 2:
+                    p2Pokemon = pickOppPokemon(opponent)
+                    if p2Pokemon != 0:
+                        p2Pokemon = pokemon(p2Pokemon)
+                        if opponent == 1:
+                            p2Pokemon.isComputer = False
+                            p2Pokemon.playerName = 'Rival'
+                            clear()
+                        else:
+                            p2Pokemon.isComputer = True
+                            p2Pokemon.playerName = 'Computer'
+                        battleStart = True
+                        validOpponent = True
             battleStart = True
         else:
             battleStart = False
-            compPokemon = 0
+            p2Pokemon = 0
 
-    return battleStart, userPokemon, compPokemon
+    return battleStart, p1Pokemon, p2Pokemon
 
 # Select opponent's pokemon
-def pickCompPokemon(userPokemon):
+def pickOppPokemon(opponent):
     clear()
-    userPokemon = pokemon(userPokemon)
-    if userPokemon.name == 'Bulbasaur':
-        compPokemon = random.choice(['Charmander', 'Squirtle'])
-    elif userPokemon.name == 'Charmander':
-        compPokemon = random.choice(['Bulbasaur', 'Squirtle'])
+    if opponent == 1:
+        print('Hello Rival! Select your pokemon:\n')
+        p2Input = input('1. Bulbasaur\n2. Charmander\n3. Squirtle\nEnter your chosen pokemon\'s number: ')
+        if p2Input == '1':
+            p2Pokemon = 'Bulbasaur'
+        elif p2Input == '2':
+            p2Pokemon = 'Charmander'
+        elif p2Input == '3':
+            p2Pokemon = 'Squirtle'
+        else:
+            p2Pokemon = 0
     else:
-        compPokemon = random.choice(['Charmander', 'Bulbasaur'])
-    compPokemon = pokemon(compPokemon)
-    return userPokemon, compPokemon
+        p2Pokemon = random.choice(['Bulbasaur', 'Charmander', 'Squirtle'])
+    return p2Pokemon
 
 # Get a move
-def selectMove(currPlayer, userPokemon, compPokemon):
-    if currPlayer == 'user':
-        currMoves = userPokemon.moves
-        print('\n\nSelect ' + userPokemon.name + '\'s move:\n')
+def selectMove(currPlayer):
+    if currPlayer.isComputer == False:
+        currMoves = currPlayer.moves
+        print('\n\n' + currPlayer.playerName + '\'s turn. Select ' + currPlayer.name + '\'s move:\n')
         index = 1
         for x in currMoves:
             print('  ' + str(index) + '. ' + x)
@@ -62,8 +87,8 @@ def selectMove(currPlayer, userPokemon, compPokemon):
         if selectedMove <= len(currMoves):
             validMove = True
             move = currMoves[selectedMove - 1]
-            moveDamage = userPokemon.moveDamage[selectedMove - 1]
-            moveText = userPokemon.moveText[selectedMove - 1]
+            moveDamage = currPlayer.moveDamage[selectedMove - 1]
+            moveText = currPlayer.moveText[selectedMove - 1]
         else:
             validMove = False
             move = 'Illegal'
@@ -71,117 +96,74 @@ def selectMove(currPlayer, userPokemon, compPokemon):
             moveText = 'Illegal'
     else:
         validMove = True
-        move = random.choice(compPokemon.moves)
-        moveDamage = compPokemon.moveDamage[compPokemon.moves.index(move)]
-        moveText = compPokemon.moveText[compPokemon.moves.index(move)]
+        move = random.choice(currPlayer.moves)
+        moveDamage = currPlayer.moveDamage[currPlayer.moves.index(move)]
+        moveText = currPlayer.moveText[currPlayer.moves.index(move)]
     return validMove, move, moveDamage, moveText
 
-def doDamage(currPlayer, userPokemon, compPokemon, move, moveDamage, moveText):
+def doDamage(currPlayer, otherPlayer, move, moveDamage, moveText):
     clear()
-    if currPlayer == 'user':
-        # If move is > 2 it is a normal attack
-        if moveDamage > 2:
-            modifiedMoveDamage = round(moveDamage * (1 + userPokemon.attMod - compPokemon.defMod))
-            compPokemon.health -= modifiedMoveDamage
-            print(userPokemon.name + ' used ' + move + '.\n' + userPokemon.name + moveText + '\nIt did ' + str(modifiedMoveDamage) + ' damage.\n')
-        # If moveDamage fits in this category it is modifier to pokemon's attack modifier
-        elif moveDamage <= 2 and moveDamage > 1:
-            if userPokemon.attMod < 2:
-                userPokemon.attMod += (moveDamage - 1)
-                if userPokemon.attMod > 2:
-                    userPokemon.attMod = 2
-                print(userPokemon.name + ' used ' + move + '.\n' + userPokemon.name + moveText + '\n')
-            else:
-                print('** ' + userPokemon.name + '\'s attack cannot go any higher **')
-        # If move damage fits in this category it is a modifier to pokemon's defense modifier
-        elif moveDamage <= 1 and moveDamage > 0:
-            if userPokemon.defMod < 2:
-                userPokemon.defMod += (moveDamage)
-                if userPokemon.defMod > 2:
-                    userPokemon.defMod = 2
-                print(userPokemon.name + ' used ' + move + '.\n' + userPokemon.name + moveText + '\n')
-            else:
-                print('** ' + userPokemon.name + '\'s defense cannot go any higher **')
-        # If move damage fits in this category it is a modifier to opponent's defense modifier
-        elif moveDamage <= 0 and moveDamage > -1:
-            if compPokemon.defMod > 0:
-                compPokemon.defMod += (moveDamage)
-                if compPokemon.defMod < 0:
-                    compPokemon.defMod = 0
-                print(userPokemon.name + ' used ' + move + '.\n' + userPokemon.name + moveText + '\n')
-            else:
-                print('** ' + compPokemon.name + '\'s defense cannot go any lower **')
-        # If move damage fits in this category it is a modifier to opponent's attack modifier
-        elif moveDamage <= -1 and moveDamage > -2:
-            if compPokemon.attMod > 0:
-                compPokemon.attMod += (moveDamage + 1)
-                if compPokemon.attMod < 0:
-                    compPokemon.attMod = 0
-                print(userPokemon.name + ' used ' + move + '.\n' + userPokemon.name + moveText + '\n')
-            else:
-                print('** ' + compPokemon.name + '\'s attack cannot go any higher **')
-    else:
-        # If move is > 2 it is a normal attack
-        if moveDamage > 2:
-            modifiedMoveDamage = round(moveDamage * (1 + userPokemon.attMod - compPokemon.defMod))
-            userPokemon.health -= modifiedMoveDamage
-            print(compPokemon.name + ' used ' + move + '.\n' + compPokemon.name + moveText + '\nIt did ' + str(modifiedMoveDamage) + ' damage.\n')
-        # If moveDamage fits in this category it is modifier to pokemon's attack modifier
-        elif moveDamage <= 2 and moveDamage > 1:
-            if compPokemon.attMod < 2:
-                compPokemon.attMod += (moveDamage - 1)
-                if compPokemon.attMod > 2:
-                    compPokemon.attMod = 2
-                print(compPokemon.name + ' used ' + move + '.\n' + compPokemon.name + moveText + '\n')
-            else:
-                print('** ' + compPokemon.name + '\'s attack cannot go any higher **')
-        # If move damage fits in this category it is a modifier to pokemon's defense modifier
-        elif moveDamage <= 1 and moveDamage > 0:
-            if compPokemon.defMod < 2:
-                compPokemon.defMod += (moveDamage)
-                if compPokemon.defMod > 2:
-                    compPokemon.defMod = 2
-                print(compPokemon.name + ' used ' + move + '.\n' + compPokemon.name + moveText + '\n')
-            else:
-                print('** ' + compPokemon.name + '\'s defense cannot go any higher **')
-        # If move damage fits in this category it is a modifier to opponent's defense modifier
-        elif moveDamage <= 0 and moveDamage > -1:
-            if userPokemon.defMod > 0:
-                userPokemon.defMod += (moveDamage)
-                if userPokemon.defMod < 0:
-                    userPokemon.defMod = 0
-                print(compPokemon.name + ' used ' + move + '.\n' + compPokemon.name + moveText + '\n')
-            else:
-                print('** ' + userPokemon.name + '\'s defense cannot go any lower **')
-        # If move damage fits in this category it is a modifier to opponent's attack modifier
-        elif moveDamage <= -1 and moveDamage > -2:
-            if userPokemon.attMod > 0:
-                userPokemon.attMod += (moveDamage + 1)
-                if userPokemon.attMod < 0:
-                    userPokemon.attMod = 0
-                print(compPokemon.name + ' used ' + move + '.\n' + compPokemon.name + moveText + '\n')
-            else:
-                print('** ' + userPokemon.name + '\'s attack cannot go any higher **')
+    # If move is > 2 it is a normal attack
+    if moveDamage > 2:
+        modifiedMoveDamage = round(moveDamage * (1 + currPlayer.attMod - otherPlayer.defMod))
+        otherPlayer.health -= modifiedMoveDamage
+        print(currPlayer.name + ' used ' + move + '.\n' + currPlayer.name + moveText + '\nIt did ' + str(modifiedMoveDamage) + ' damage.\n')
+    # If moveDamage fits in this category it is modifier to pokemon's attack modifier
+    elif moveDamage <= 2 and moveDamage > 1:
+        if currPlayer.attMod < 2:
+            currPlayer.attMod += (moveDamage - 1)
+            if currPlayer.attMod > 2:
+                currPlayer.attMod = 2
+            print(currPlayer.name + ' used ' + move + '.\n' + currPlayer.name + moveText + '\n')
+        else:
+            print('** ' + currPlayer.name + '\'s attack cannot go any higher **')
+    # If move damage fits in this category it is a modifier to pokemon's defense modifier
+    elif moveDamage <= 1 and moveDamage > 0:
+        if currPlayer.defMod < 2:
+            currPlayer.defMod += (moveDamage)
+            if currPlayer.defMod > 2:
+                currPlayer.defMod = 2
+            print(currPlayer.name + ' used ' + move + '.\n' + currPlayer.name + moveText + '\n')
+        else:
+            print('** ' + currPlayer.name + '\'s defense cannot go any higher **')
+    # If move damage fits in this category it is a modifier to opponent's defense modifier
+    elif moveDamage <= 0 and moveDamage > -1:
+        if otherPlayer.defMod > 0:
+            otherPlayer.defMod += (moveDamage)
+            if otherPlayer.defMod < 0:
+                otherPlayer.defMod = 0
+            print(currPlayer.name + ' used ' + move + '.\n' + currPlayer.name + moveText + '\n')
+        else:
+            print('** ' + otherPlayer.name + '\'s defense cannot go any lower **')
+    # If move damage fits in this category it is a modifier to opponent's attack modifier
+    elif moveDamage <= -1 and moveDamage > -2:
+        if otherPlayer.attMod > 0:
+            otherPlayer.attMod += (moveDamage + 1)
+            if otherPlayer.attMod < 0:
+                otherPlayer.attMod = 0
+            print(currPlayer.name + ' used ' + move + '.\n' + currPlayer.name + moveText + '\n')
+        else:
+            print('** ' + otherPlayer.name + '\'s attack cannot go any higher **')
 
-def checkHealth(userPokemon, compPokemon):
-    if userPokemon.health <= 0:
-        print(userPokemon.name + ' has fainted. ' + compPokemon.name + ' is the winner!')
+def checkHealth(currPlayer, otherPlayer):
+    if currPlayer.health <= 0:
+        print(currPlayer.name + ' has fainted. ' + otherPlayer.playerName + ' is the winner!')
         gameOver = True
-    elif compPokemon.health <= 0:
-        print(compPokemon.name + ' has fainted. ' + userPokemon.name + ' is the winner!')
+    elif otherPlayer.health <= 0:
+        print(otherPlayer.name + ' has fainted. ' + currPlayer.playerName + ' is the winner!')
         gameOver = True
     else:
         gameOver = False
     return gameOver
 
-def endTurn(currPlayer):
+def endTurn(currPlayer, otherPlayer):
     input('Input anything to continue to the next turn: ')
     clear()
-    if currPlayer == 'user':
-        currPlayer = 'comp'
-    else:
-        currPlayer = 'user'
-    return currPlayer
+    tempVar1 = currPlayer
+    tempVar2 = otherPlayer
+    currPlayer = tempVar2
+    otherPlayer = tempVar1
+    return currPlayer, otherPlayer
 
 
 
--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import functions
 battleStart = False
 gameOver = False
-userPokemon = 0
-compPokemon = 0
-currPlayer = 'user'
+playerOne = 0
+playerTwo = 0
+currPlayer = 0
+otherPlayer = 0
 
 # Start the game
 while battleStart == False:
-    battleStart, userPokemon, compPokemon = functions.gameStart()
+    battleStart, playerOne, playerTwo = functions.gameStart()
+
+# Assign the current player and other player
+currPlayer = playerOne
+otherPlayer = playerTwo
 
 # Start the battle
 while gameOver == False:
-    formattedUser = '{:.0f}'.format(userPokemon.health)
-    formattedComp = '{:.0f}'.format(compPokemon.health)
-    print(userPokemon.name + '\n  Health: ' + formattedUser + '\n' + compPokemon.name + '\n  Health: ' + formattedComp)
+    formattedP1 = '{:.0f}'.format(playerOne.health)
+    formattedP2 = '{:.0f}'.format(playerTwo.health)
+    print(playerOne.name + '\n  Health: ' + formattedP1 + '\n' + playerTwo.name + '\n  Health: ' + formattedP2)
     # Have the current player select a move
-    validMove, move, moveDamage, moveText = functions.selectMove(currPlayer, userPokemon, compPokemon)
+    validMove, move, moveDamage, moveText = functions.selectMove(currPlayer)
     # If the move is valid, have it deal damage. If not, ask for move again
     if validMove == True:
         # Deal the damage to the opposing pokemon
-        functions.doDamage(currPlayer, userPokemon, compPokemon, move, moveDamage, moveText)
+        functions.doDamage(currPlayer, otherPlayer, move, moveDamage, moveText)
         # Check if the game is over
-        gameOver = functions.checkHealth(userPokemon, compPokemon)
+        gameOver = functions.checkHealth(currPlayer, otherPlayer)
         # If the game isn't over swap the current player
         if gameOver == False:
-            currPlayer = functions.endTurn(currPlayer)
+            currPlayer, otherPlayer = functions.endTurn(currPlayer, otherPlayer)
